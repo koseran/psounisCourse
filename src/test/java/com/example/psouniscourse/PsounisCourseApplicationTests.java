87b4d8c2fdbb61f5df2611f7bc8d211a06edcb6e
koseran <kostassarantavgas2011@gmail.com>
2023-12-04 21:11:01 +0200

    Initial commit

---
+++ src/test/java/com/example/psouniscourse/PsounisCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.psouniscourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsounisCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
